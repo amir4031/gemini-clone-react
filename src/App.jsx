c0bf3a67e8071dde77f66ca60d6f823e02a04a2b
amir-ansari <amiransari3550@gmail.com>
2024-10-16 19:03:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './compnonents/Sidebar/Sidebar'
+import Main from './compnonents/Main/Main'
+
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
+
